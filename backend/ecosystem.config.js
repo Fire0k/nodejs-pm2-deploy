d8bec875552954d07aa951892e0d9943102e0af9
a.mizerny <a.mizerny@kryptonite.ru>
2025-11-04 00:07:42 +0300

    fixes

---
+++ backend/ecosystem.config.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+
+const {
+  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF = 'master',
+} = process.env;
+
+module.exports = {
+  apps: [{
+    name: 'app',
+    script: './src/app.ts',
+  }],
+
+  // Настройка деплоя
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: 'https://github.com/Fire0k/nodejs-pm2-deploy.git',
+      path: DEPLOY_PATH,
+      'pre-deploy': `scp ./*.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      'post-deploy': 'npm i && npm run build',
+    },
+  },
+};
