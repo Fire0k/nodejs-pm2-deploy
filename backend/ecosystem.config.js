65b9299201088b6c8535c6d85b15775096600c31
a.mizerny <a.mizerny@kryptonite.ru>
2025-11-04 01:53:35 +0300

    fix

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,25 +1,39 @@
 require('dotenv').config();
 
 const {
-  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF = 'master',
+  DEPLOY_USER,
+  DEPLOY_HOST,
+  DEPLOY_PATH,
+  DEPLOY_REPO,
+  DEPLOY_REF = 'main', // или master
 } = process.env;
 
 module.exports = {
-  apps: [{
-    name: 'app',
-    script: './src/app.ts',
-  }],
+  apps: [
+    {
+      name: 'backend',
+      script: 'dist/app.js', // или путь к точке входа
+      env: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
 
-  // Настройка деплоя
   deploy: {
     production: {
       user: DEPLOY_USER,
       host: DEPLOY_HOST,
       ref: DEPLOY_REF,
-      repo: 'https://github.com/Fire0k/nodejs-pm2-deploy.git',
-      path: DEPLOY_PATH,
-      'pre-deploy': `scp ./*.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
-      'post-deploy': 'npm i && npm run build',
+      repo: DEPLOY_REPO,
+      path: `${DEPLOY_PATH}`,
+      'pre-deploy-local': `scp ./.env.deploy ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/backend/.env`,
+      'post-deploy': `
+        export NVM_DIR="$HOME/.nvm" &&
+        source "$NVM_DIR/nvm.sh" &&
+        npm install &&
+        npm run build &&
+        pm2 reload ecosystem.config.js --env production
+      `,
     },
   },
 };
