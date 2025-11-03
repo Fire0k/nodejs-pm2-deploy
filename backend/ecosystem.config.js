b5e51a65182654dc63ea676e4195bf571d9169df
a.mizerny <a.mizerny@kryptonite.ru>
2025-11-04 02:23:52 +0300

    fix

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -4,15 +4,16 @@ const {
   DEPLOY_USER,
   DEPLOY_HOST,
   DEPLOY_PATH,
+  DEPLOY_REF,
   DEPLOY_REPO,
-  DEPLOY_REF = 'main', // или master
 } = process.env;
 
 module.exports = {
   apps: [
     {
       name: 'backend',
-      script: 'dist/app.js', // или путь к точке входа
+      script: 'npm',
+      args: 'run start',
       env: {
         NODE_ENV: 'production',
       },
@@ -25,12 +26,14 @@ module.exports = {
       host: DEPLOY_HOST,
       ref: DEPLOY_REF,
       repo: DEPLOY_REPO,
-      path: `${DEPLOY_PATH}`,
-      'pre-deploy-local': `scp ./.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/backend`,
+      path: DEPLOY_PATH,
+      'pre-deploy-local': 'echo "Copying env file..."',
+      'pre-deploy': `scp ./.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/source/backend`,
       'post-deploy': `
         export NVM_DIR="$HOME/.nvm" &&
         source "$NVM_DIR/nvm.sh" &&
-        npm install &&
+        cd ${DEPLOY_PATH}/source/backend &&
+        npm ci &&
         npm run build &&
         pm2 reload ecosystem.config.js --env production
       `,
