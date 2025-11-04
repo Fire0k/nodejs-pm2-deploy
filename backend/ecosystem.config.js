fd1cb3c8b9fa539992252e10cc28b8ee48822cd0
a.mizerny <a.mizerny@kryptonite.ru>
2025-11-04 21:10:02 +0300

    fix path

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -32,6 +32,9 @@ module.exports = {
       ref: `origin/${DEPLOY_REF}`,
       repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
+      env: {
+        PATH: '/home/fireok/.nvm/versions/node/v16.20.2/bin/node', // <-- путь из `which`
+      },
       'pre-deploy-local': `scp .env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/shared/.env || true && scp .env.deploy ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/shared/.env.deploy || true`,
       'post-setup': `mkdir -p ${DEPLOY_PATH}/shared && mkdir -p ${DEPLOY_PATH}/logs`,
       'post-deploy': `cp ${DEPLOY_PATH}/shared/.env . || true && cp ${DEPLOY_PATH}/shared/.env.deploy . || true && npm ci && npm run build && pm2 reload ${DEPLOY_PATH}/current/ecosystem.config.js --env production || pm2 start ecosystem.config.js --env production`,
